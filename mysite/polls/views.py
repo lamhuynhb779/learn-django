f5e114fec4337e7577cb542be5024439bdf2f3df
lamhuynhb779 <lamhuynhb779@gmail.com>
2021-01-09 22:09:47 +0700

    add template question_list + function base view question list + url question list

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 
 def index(req):
-    #return HttpResponse("Hello cac ban!!!")
-    myname = 'Lam Huynh'
+    # return HttpResponse("Hello cac ban!!!")
+    my_name = 'Lam Huynh'
     info = ["25 years old", "backend developer", "Love technologies"]
-    return render(req, "polls/index.html", {"name": myname, "personal": info})
+    return render(req, "polls/index.html", {"name": my_name, "personal": info})
+
+
+def showQuestions(req):
+    # questions = Question.objects.all()
+    questions = Question.objects.all()
+    data_questions = {"questions": questions}
+    return render(req, "polls/question_list.html", data_questions)
