e89a9ee4908759be00d6517797c430b299c8c532
lamhuynhb779 <lamhuynhb779@gmail.com>
2021-01-10 23:43:07 +0700

    add attribute html tag by widgets

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -6,12 +6,16 @@ from .models import Question
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ('question_text',)
+        fields = ("question_text",)
+        # Add widgets
+        widgets = {
+            "question_text": forms.TextInput(attrs={"id": "question-text"}),
+        }
 
 
 # Class extends Form
 class SendMail (forms.Form):
-    title = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea)
-    cc = forms.BooleanField(required=False)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "mail-item", "id": "mail-title"}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={"class": "mail-item"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "mail-item", "id": "mail-content"}))
+    cc = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={"checked": True, "data-cc": "cc"}))
